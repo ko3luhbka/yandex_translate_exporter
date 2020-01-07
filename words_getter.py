7635d96a0812912004ffbe011bf20390278e4841
ko3luhbka <ko3luhbka@gmail.com>
2020-01-07 14:43:27 +0300

    Move all constants to words_getter.py, get rid on config file and argparse

--- words_getter.py
+++ words_getter.py
@@ -1,3 +1,5 @@
+import csv
+import os
 import time
 
 from bs4 import BeautifulSoup
@@ -7,19 +9,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-import config
+# Path to Selenium Webdriver
+DRIVER_PATH = os.path.abspath('geckodriver')
+# Credentials to authenticate to Yandex.Translate service
+EMAIL = ''
+PASSWORD = ''
+# File where parsed page is saved to
+HTML_SOURCE = 'page_source.html'
+# Resulting CSV file
+CSV_FILE = 'word_trans.csv'
+# Yandex.Translate options
+BASE_URL = 'https://translate.yandex.ru/collections'
+COLLECTION_NAME = 'my'  # Words collection which must be proceeded
 
 
 def init_webdriver():
     options = webdriver.FirefoxOptions()
     options.add_argument('--ignore-certificate-errors')
-    return webdriver.Firefox(executable_path=config.driver_path)
+    return webdriver.Firefox(executable_path=DRIVER_PATH)
 
 
 def _fill_in_and_hit_return(
-    driver: webdriver.Firefox,
-    elem_name: str,
-    field_value: str,
+    driver,
+    elem_name,
+    field_value,
 ):
     wait = WebDriverWait(driver, 10)
     wait.until(expected_conditions.presence_of_element_located(
@@ -30,16 +43,16 @@ def _fill_in_and_hit_return(
     elem.send_keys(Keys.RETURN)
 
 
-def auth_via_google(
+def auth_with_google(
     driver,
-    email=config.email,
-    password=config.password
+    email=EMAIL,
+    password=PASSWORD
 ):
     """
     I don't have Yandex account so authenticate with Google
     """
     wait = WebDriverWait(driver, 10)
-    driver.get(config.base_url)
+    driver.get(BASE_URL)
     driver.find_element_by_class_name('button_enter').click()
     wait.until(expected_conditions.element_to_be_clickable(
         (By.CLASS_NAME, 'passp-social-block__list-item-gg')
@@ -54,9 +67,9 @@ def auth_via_google(
             driver.switch_to.window(window_handle)
             break
 
-    _fill_in_and_hit_return(driver, 'identifier', config.email)
+    _fill_in_and_hit_return(driver, 'identifier', email)
     time.sleep(2)
-    _fill_in_and_hit_return(driver, 'password', config.password)
+    _fill_in_and_hit_return(driver, 'password', password)
 
     wait.until(expected_conditions.number_of_windows_to_be(1))
     driver.switch_to.window(original_window)
@@ -65,12 +78,12 @@ def auth_via_google(
 
 def get_html_source(
     driver,
-    collection_name=config.collection_name
+    collection_name=COLLECTION_NAME
 ):
     """
     Load page with user defined words collections and get its source code
     """
-    auth_via_google(driver=driver)
+    auth_with_google(driver=driver)
     users_collections = driver.find_element_by_id('collectionListContent')
     collections = users_collections.find_element_by_tag_name('ul')
     for coll in collections.find_elements_by_tag_name('li'):
@@ -107,3 +120,40 @@ def parse_html(html_file):
             }
         )
     return word_trans_list
+
+
+def convert_dict_to_csv(words_dict, csv_file=CSV_FILE):
+    """
+    Convert {word: translation} dictionary to CSV
+    """
+    if os.path.exists(csv_file):
+        print('CSV file is already exist, exiting')
+        return
+    with open(csv_file, 'w') as f:
+        fieldnames = ['word', 'translation']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for item in words_dict:
+            writer.writerow(item)
+
+
+def main(html_source=HTML_SOURCE):
+    if not all([EMAIL, PASSWORD]):
+        raise ValueError(
+            'Email and password should not be empty!\n'
+            'Either specify them in config.py or in script positional args'
+        )
+    if os.path.exists(html_source):
+        print('HTML file is already exist, skipping page scrapping')
+    else:
+        driver = init_webdriver()
+        page_source = get_html_source(driver)
+        with open(html_source, 'w') as f:
+            f.write(page_source)
+
+    source_parsed = parse_html(html_source)
+    convert_dict_to_csv(source_parsed)
+
+
+if __name__ == '__main__':
+    main()
